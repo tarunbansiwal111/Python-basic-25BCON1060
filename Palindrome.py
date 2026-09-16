6b9f5191dd35ca3de27ed4f6876d27e3a65c1759
tarunbansiwal111 <tarunbansiwal111@gmail.com>
2026-09-16 22:07:58 +0530

    FEAT:ADD PALINDROME

---
+++ Palindrome.py
@@ -0,0 +1,14 @@
+n = int(input("Enter a number: "))
+
+original = n
+reverse = 0
+
+while n > 0:
+    digit = n % 10
+    reverse = reverse * 10 + digit
+    n = n // 10
+
+if original == reverse:
+    print("Palindrome number")
+else:
+    print("Not a palindrome number")
